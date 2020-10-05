8b28c40767ecba4c51e4ac2944f19f5d5a82e275
Benjamin West <23172276+bgwest@users.noreply.github.com>
2020-10-04 20:45:04 -0700

    added all textures into game.cs

--- Game1.cs
+++ Game1.cs
@@ -9,17 +9,32 @@ namespace konoha
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D player_Sprite;
+        Texture2D playerDown;
+        Texture2D playerUp;
+        Texture2D playerLeft;
+        Texture2D playerRight;
+
+        Texture2D eyeEnemy_Sprite;
+        Texture2D snakeEnemy_Sprite;
+        Texture2D bush_Sprite;
+        Texture2D tree_Sprite;
+
+        Texture2D heart_Sprite;
+        Texture2D bullet_Sprite;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
@@ -27,7 +42,20 @@ namespace konoha
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            player_Sprite = Content.Load<Texture2D>("player/player");
+            playerUp = Content.Load<Texture2D>("player/playerUp");
+            playerRight = Content.Load<Texture2D>("player/playerRight");
+            playerDown = Content.Load<Texture2D>("player/playerDown");
+            playerLeft = Content.Load<Texture2D>("player/playerLeft");
+
+            eyeEnemy_Sprite = Content.Load<Texture2D>("enemies/eyeEnemy");
+            snakeEnemy_Sprite = Content.Load<Texture2D>("enemies/snakeEnemy");
+
+            bush_Sprite = Content.Load<Texture2D>("obstacles/bush");
+            tree_Sprite = Content.Load<Texture2D>("obstacles/tree");
+
+            bullet_Sprite = Content.Load<Texture2D>("misc/bullet");
+            heart_Sprite = Content.Load<Texture2D>("misc/heart");
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +63,12 @@ namespace konoha
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.ForestGreen);
 
             base.Draw(gameTime);
         }
